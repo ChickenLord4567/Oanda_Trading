68cfa9c7abc459023ecbe5d6d3f5738524eca240
ChickenLord4567 <132816835+ChickenLord4567@users.noreply.github.com>
2025-07-31 22:34:52 +0100

    Update App.tsx

--- QuantumTrade-1/client/src/App.tsx
+++ QuantumTrade-1/client/src/App.tsx
@@ -48,15 +48,5 @@ function Router() {
   );
 }
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
 
 export default App;
